df520af7a5375dfb54ece5943ef669712c3c131c
JiwonLeeeeeeee <eziwon0228@gmail.com>
2023-03-14 15:35:18 +0900

    .

--- hw1.cpp
+++ hw1.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
 using namespace std;
 
+class Heap {
+public:
+    char name[10];
+    int id[4];
+    char school[20];
+};
+
+//insert를 위한 정보를 입력받는 function
+void getI() {
+    char n_name[10];
+    int n_id;
+    char n_school[20];
+    cout << "Enter name of element: " ;
+    cin >> n_name ;
+    cout << "Enter id of element: ";
+    cin >> n_id;
+    cout << "Enter school of element: ";
+    cin >> n_school;
+}
+
+void insert(Heap h[], Heap k) {
+
+}
+
 int main() {
-    char m;
+    char m;     //입력 받을 menu
+    Heap que[30];
     
     while(1) {
         cout << "*********** MENU ****************" << endl;
@@ -17,6 +42,9 @@ int main() {
         // Q를 입력받은 경우 반복 중지
         if (m == 'Q') break;
         // if (m == 'I') 삽입하는 함수 호출
+        if (m == 'I') {
+            getI();
+        }
     }
     
     cout << "Thank you, Bye!" << endl;
